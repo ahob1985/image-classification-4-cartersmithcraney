3d53530c3756d458976bd7ea1ccbae44faf3a0e2
Carter Smith <67392536+cartersmithcraney@users.noreply.github.com>
2021-06-23 18:22:51 +0000

    Updated code (not finished)

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-// Author:
+// Author: Carter Smith Craney
 
-// Global UI Variables
-//let canvasDiv;
+// Global UI Variables 
+let canvasDiv; 
+let canvas; 
+let textDiv; 
+let textP; 
+let buttonDiv;
+let submitButton; 
+let resetButton;
 
-// Global ML Variables
-//let doodlenet;
+// Global ML Variables 
+let doodlenet; 
+let img; 
+let isModelReady;
+
 
 function setup() {
+canvasDiv= createDiv(); 
+canvas = createCanvas(640, 480); 
+canvas.parent(canvasDiv);textDiv= createDiv; 
+textP = createP("Model loading, please wait..."); 
+textP.parent(textDiv); 
+//create button UI 
+buttonDiv = createDiv(); 
+submitButton = createButton("SUBMIT");
+submitButton.parent(buttonDiv);
+submitButton.mousePressed(predictImage);
+resetButton = createButton("RESET");
+resetButton.parent(buttonDiv);
+resetButton.mousePressed(resetCanvas); 
+buttonDiv.style("display", "none"); 
+//initialize ML Variables 
+isModelReady = false; 
+doodlenet = ml5.imageClassifier("DoodleNet", modelReady);
 
 }
 
 function draw() {
+if(mouseIsPressed && isModelReady) {
+  strokeWeight(25);
+  line(mouseX, mouseY, pmouseX, pmouseY);
 
+}
 }
 
 function resetCanvas() {
@@ -19,6 +49,9 @@ function resetCanvas() {
 }
 
 function modelReady() {
+isModelReady = true; 
+buttonDiv.style("display", "block");
+textP.html("Draw your image, then click submit!");
 
 }
 
